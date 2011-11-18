d28c6a4326e046ad0ab5fd58ca5ae8a5662b227d
CJ Kucera <pez@apocalyptech.com>
2011-11-18 11:55:58 -0600

    prep for 3.5.0 release

--- index.php
+++ index.php
@@ -36,12 +36,13 @@ or to the
 
 <strong>Current Version</strong>
 <blockquote>
-<? xray_rel('3.4.3', 'October 21, 2011'); ?>
+<? xray_rel('3.5.0', 'November 18, 2011'); ?>
 </blockquote>
 
 <img src="plus.png" id="previoustoggle" onClick="togglePrev();"> <strong onClick="togglePrev();">Previous Versions</strong>
 <div id="previous" style="display: none;">
 <blockquote>
+<? xray_rel('3.4.3', 'October 21, 2011'); ?>
 <? xray_rel('3.4.2', 'October 20, 2011'); ?>
 <? xray_rel('3.4.1', 'October 15, 2011'); ?>
 <? xray_rel('3.4.0', 'October 11, 2011'); ?>
@@ -162,7 +163,7 @@ find your way out if you're lost.</p>
 taken place since X-Ray 2.7, but the biggest major changes have been:
 
 <ul>
-    <li>Full support for all blocks through Beta 1.9-pre5.  This includes Nether blocks, Lapis Lazuli, Note Blocks, Beds, Trapdoors, Pistons, Huge Mushrooms, Iron Bars, Enchantment Tables, etc.</li>
+    <li>Full support for all blocks through Minecraft 1.0-rc2.  This includes Nether blocks, Lapis Lazuli, Note Blocks, Beds, Trapdoors, Pistons, Huge Mushrooms, Iron Bars, Enchantment Tables, etc.</li>
     <li>Blocks are loaded on-demand, and the app should handle worlds of any size.  This also results in much faster initial load times.</li>
     <li>Keybindings and resource highlight settings can be specified in a configuration file.</li>
     <li>"Explored" highlighting, which shows you where you've placed torches in underground tunnels.</li>
